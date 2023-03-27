6a1d3a82d346528e933a64b8b055591c84455df1
shayashi <shayashi@mathworks.com>
2023-03-27 15:41:23 +0900

    for debug

--- Utility/gitutil/extractLastArtifacts.m
+++ Utility/gitutil/extractLastArtifacts.m
@@ -2,7 +2,7 @@ function compMdlName = extractLastArtifacts(branch, mdlname, opRemote)
 % safe directory追加（gitの仕様変更に伴う対応)
 myProject       = matlab.project.currentProject;
 projectRoot     = myProject.RootFolder;
-cmd = ['git config --global --add safe.directory ', projectRoot.char];
+cmd = ['git config --global --add safe.directory ', projectRoot.char]
 [st, log] = system(cmd);
 
 % 直近のcomit id取得
@@ -11,7 +11,7 @@ if(opRemote)
 else
     strBranch = branch;
 end
-cmd = ['git log ', strBranch, ' -n 1'];     
+cmd = ['git log ', strBranch, ' -n 1']     
 [~, log_txt] = system(cmd);
 log_txt = strsplit(log_txt,{' ', '\n'});     % 空白と改行で文字列を区切る
 commit_id = log_txt{2};                       %２番目の要素がコミットID
